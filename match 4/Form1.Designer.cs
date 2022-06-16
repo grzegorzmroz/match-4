0f405780e1b3974f24de45df6bdb35d5db37577d
grzegorzmroz <kapitan.prix@gmail.com>
2022-06-16 15:25:52 +0200

    Add project files.

---
+++ match 4/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace match_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbGame = new System.Windows.Forms.GroupBox();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gbGame
+            // 
+            this.gbGame.Location = new System.Drawing.Point(146, 12);
+            this.gbGame.Name = "gbGame";
+            this.gbGame.Size = new System.Drawing.Size(595, 510);
+            this.gbGame.TabIndex = 0;
+            this.gbGame.TabStop = false;
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(30, 81);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(0, 20);
+            this.lblWinner.TabIndex = 1;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(24, 22);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(86, 30);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 535);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.gbGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGame;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ match 4/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace match_4
+{
+    public partial class Form1 : Form
+    {
+        public Button[][] buttons = new Button[7][];
+        public enum Player
+        {
+            red,
+            yellow
+        }
+        public Player player = Player.red;
+        public bool winner = false;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                Button[] buttonscol = new Button[6];
+                for (int j = 0; j < 6; j++)
+                {
+                    Button newbutton = new Button();
+                    newbutton.Location = new System.Drawing.Point(85 * i, 85 * j);
+                    newbutton.Name = $"newbtn{i}{j}";
+                    newbutton.Size = new System.Drawing.Size(85, 85);
+                    newbutton.TabIndex = i + j;
+                    newbutton.Text = String.Empty;
+                    newbutton.UseVisualStyleBackColor = true;
+                    newbutton.BackColor = Color.White;
+                    newbutton.Click += new EventHandler(BoardClick);
+                    buttonscol[j] = newbutton;
+                    gbGame.Controls.Add(newbutton);
+                }
+                buttons[i] = buttonscol;
+            }
+
+        }
+        private void BoardClick(object sender, EventArgs e)
+        {
+            if (winner == false)
+            {
+                Button clicked = sender as Button;
+                int col = 0;
+                for (int i = 0; i < 7; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (buttons[i][j] == clicked) col = i;
+                    }
+                }
+                for (int i = 5; i >= 0; i--)
+                {
+                    if (buttons[col][i].BackColor == Color.White)
+                    {
+                        if (player == Player.red)
+                        {
+                            buttons[col][i].BackColor = Color.Red;
+                            if (GetWinner())
+                            {
+                                winner = true;
+                                lblWinner.Text = "Red wins";
+                            }
+                            player = Player.yellow;
+                            break;
+                        }
+                        if (player == Player.yellow)
+                        {
+                            buttons[col][i].BackColor = Color.Yellow;
+                            if (GetWinner())
+                            {
+                                winner = true;
+                                lblWinner.Text = "Yellow wins";
+                            }
+                            player = Player.red;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        public bool GetWinner()
+        {
+            int win ;
+            //checking for winners vertically
+            for (int i = 0; i < 7; i++)
+            {
+                win = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttons[i][j].BackColor == buttons[i][j + 1].BackColor && buttons[i][j].BackColor != Color.White)
+                    {
+                        win++;
+                        if (win == 3)
+                        {
+                            return true;
+                        }
+                    }
+                    else win = 0;
+                }
+            }
+            //checking for winners horizontally
+            for (int i = 0; i < 6; i++)
+            {
+                win = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    if (buttons[j][i].BackColor == buttons[j + 1][i].BackColor && buttons[j][i].BackColor != Color.White)
+                    {
+                        win++;
+                        if (win == 3)
+                        {
+                            return true;
+                        }
+                    }
+                    else win = 0;
+                }
+            }
+            //checking for winners on descending diagonals that touch the left wall
+            for (int i = 2; i>=0;i--)
+            {
+                win = 0;
+                int k = i;
+                for (int j = 0; j<6-i-1;j++)
+                {
+                        if (buttons[j][k].BackColor == buttons[j + 1][k + 1].BackColor && buttons[j][k].BackColor != Color.White)
+                        {
+                            win++;
+                            if (win == 3) return true;
+                        }
+                        else win = 0;
+                    k++;
+                }
+            }
+            // checking for winners on descending diagonals that do not touch the left wall
+            for (int i=1; i<4;i++)
+            {
+                win = 0;
+                int k = i;
+                for(int j = 0; j<6-i;j++)
+                {
+                    if (buttons[k][j].BackColor == buttons[k+ 1][j + 1].BackColor && buttons[k][j].BackColor != Color.White)
+                    {
+                        win++;
+                        if (win == 3) return true;
+                    }
+                    else win = 0;
+                    k++;
+                }
+            }
+            //checking for winners on ascending diagonals that touch the left wall
+            for (int i=3;i<6;i++)
+            {
+                win = 0;
+                int k = i;
+                for (int j=0; j<i;j++)
+                {
+                    if (buttons[j][k].BackColor == buttons[j+1][k-1].BackColor && buttons[j][k].BackColor != Color.White)
+                    {
+                        win++;
+                        if (win == 3) return true;
+                    }
+                    else win = 0;
+                    k--;
+                }
+            }
+            // checking for winners on ascending diagonals that do not touch the left wall
+            for (int i=1;i<4;i++)
+            {
+                win = 0;
+                int k = i;
+                for(int j = 5; j>=i;j--)
+                {
+                    if (buttons[k][j].BackColor == buttons[k + 1][j - 1].BackColor && buttons[k][j].BackColor != Color.White)
+                    {
+                        win++;
+                        if (win == 3) return true;
+                    }
+                    else win = 0;
+                    k++;
+                }
+            }
+            return false;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    buttons[i][j].BackColor = Color.White;
+                }
+            }
+            player = Player.red;
+            winner = false;
+            lblWinner.Text = String.Empty;
+        }
+    }
+}
